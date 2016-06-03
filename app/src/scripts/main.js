d50123af36e3fc8c15c9687cca7afec789a679ae
Tobias Büchel <tobi.buechel@gmail.com>
2016-06-03 12:49:21 +0200

    Renamed create-note to details

--- app/src/scripts/main.js
+++ app/src/scripts/main.js
@@ -1,18 +1,18 @@
 import $ from 'jquery';
 import {home} from './home';
-import {createNote} from './create-note';
+import {details} from './details';
 
 
 //after Page is loaded
 $(function () {
-    $('#home-container').hide();
-    //$('#create-note-container').hide();
+    //$('#home-container').hide();
+    $('#details-container').hide();
     
     $('#home-container').load('/dist/app/views/home.html', function () {
         home.registerEvents();
     });
-    $('#create-note-container').load('/dist/app/views/create-note.html', function () {
-        createNote.registerEvents();
+    $('#details-container').load('/dist/app/views/details.html', function () {
+        details.registerEvents(); 
     });
 });
 
