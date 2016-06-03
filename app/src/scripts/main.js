6a76ab9d21aaf9375b5f2dd40860c74606c139b0
Tobias Büchel <tobi.buechel@gmail.com>
2016-06-03 11:44:18 +0200

    merged edit-page manually into develop

--- app/src/scripts/main.js
+++ app/src/scripts/main.js
@@ -5,10 +5,12 @@ import {createNote} from './create-note';
 
 //after Page is loaded
 $(function () {
+    //$('#home-container').hide();
+    $('#create-note-container').hide();
+    
     $('#home-container').load('/dist/app/views/home.html', function () {
         home.registerEvents();
     });
-    $('#create-note-container').hide();
     $('#create-note-container').load('/dist/app/views/create-note.html', function () {
         createNote.registerEvents();
     });
