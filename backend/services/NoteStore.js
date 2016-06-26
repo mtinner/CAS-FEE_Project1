3bed3fdc335fe1bd167bda0bf8622213ca2488e0
Tobias Büchel <tobi.buechel@gmail.com>
2016-06-26 23:31:50 +0200

    Removed dummy generator

--- backend/services/NoteStore.js
+++ backend/services/NoteStore.js
@@ -8,30 +8,11 @@ var noteStore = (function () {
         id = 0;
 
     return {
-        setNumberOfDummyNotes: setNumberOfDummyNotes,
         getNote: getNote,
         getNotes: getNotes,
         addNote: addNote,
         updateNote: updateNote
     };
-
-    function setNumberOfDummyNotes(numberOfDummyNotes) {
-        numberOfDummyNotes = parseInt(numberOfDummyNotes);
-        if (dummyNotes.length > numberOfDummyNotes) {
-            dummyNotes.splice(numberOfDummyNotes, dummyNotes.length - numberOfDummyNotes);
-        }
-        for (var i = dummyNotes.length; i < numberOfDummyNotes; i++) {
-            dummyNotes.push(
-                new Note(
-                    id++,
-                    new Date(),
-                    'title' + i,
-                    i + ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-                    Math.floor(Math.random() * (3 - 1 + 1)) + 1,
-                    !(Math.floor(Math.random() * (1 - 0 + 1)) + 0)
-                ));
-        }
-    }
 
     function getNote(id) {
         id = parseInt(id);
